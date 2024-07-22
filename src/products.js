5269ba53d15a46a6a0f627f42e047d419ebc4df7
sushant kumar <142180324+Sushiii2003@users.noreply.github.com>
2024-07-22 12:20:50 +0530

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone 14 pro",
+    price: 89999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 110999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Cannon M50 Camera",
+    price: 70499.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "H&M Denim Jacket",
+    price: 2499.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "LED Light Strips",
+    price: 1500.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Spectrum T-shirt",
+    price: 999.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Multi-Coloured Golf T-shirt",
+    price: 1150.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Sports Hat",
+    price: 550.0,
+    productImage: product8,
+  },
+];
